35b728716060d24769cfa83148b0f9d0399d05d2
JBaba <naimish101.viradia@gmail.com>
2016-12-18 14:54:31 -0500

    login page without ui added

--- DTest/webapp/urls.py
+++ DTest/webapp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.login, name='login'),
 ]
 """ Here we are calling method 'index' which is located inside 'views.py' file """
 """ We can not write comments inside list """
--- DTest/webapp/views.py
+++ DTest/webapp/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse("<h2>Hi There!</h2>")
 
+def login(request):
+	return render(request,'webapp/login.html')
+
 # Create your views here.
