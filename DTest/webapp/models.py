d0c83b155e24258777f59324a93877e406653111
JBaba <naimish101.viradia@gmail.com>
2016-12-25 11:20:13 -0500

    login user insert test

--- DTest/webapp/models.py
+++ DTest/webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+    date = models.DateTimeField()
+
+    """ To get String our of title object we need to write getter for this field """
+    def __str__(self):
+        return "User[" + self.username + " " + self.password + " " + str(self.date) + "]" 
--- DTest/webapp/views.py
+++ DTest/webapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from webapp.forms.login import submitLogin
 from django.http.response import HttpResponseRedirect
+from .models import User
+from datetime import datetime
 
 def index(request):
 	return HttpResponse("<h2>Hi There!</h2>")
@@ -10,16 +12,33 @@ def login(request):
 	return render(request,'webapp/login.html')
 
 def dologin(request):
+	if request.method == 'POST':
+		print('Login Username : ',request.POST.get('USERNAME'))
+		users = User.objects.all()
+		noOfUsers = users.__len__()
+		if noOfUsers == 0 :
+			loginUser = User()
+			loginUser.username = request.POST.get('USERNAME')
+			loginUser.password = request.POST.get('Password')
+			loginUser.date = str(datetime.now())
+			loginUser.save()
+			print loginUser
+		print users
+		print noOfUsers
+	
+	return render(request,'webapp/login.html')
+
+
+def dologinWithForms(request):
 	print('i am in do login')
 	print(request.method)
-	print(request.POST)
-	print(request.form)
+	print(request.POST.get('USERNAME'))
 	for key, value in request.POST.iteritems():
 		print(key , value)
 		
 	if request.method == 'POST':
 		form = submitLogin(request.POST)
-		print form
+		print(form)
 		
 		if form.is_valid():
 			return HttpResponseRedirect('/thanks/')
